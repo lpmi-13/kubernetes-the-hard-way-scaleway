7679bc655c6881659c52fe6714c565910565275c
Adam Leskis <leskis@gmail.com>
2024-01-24 23:31:52 +0000

    update local tooling

--- scripts/2.sh
+++ scripts/2.sh
@@ -1,22 +1,22 @@
+#! /bin/sh
 wget -q --show-progress --https-only --timestamping \
-  https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 \
-  https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+  https://github.com/cloudflare/cfssl/releases/download/v1.4.1/cfssl_1.4.1_linux_amd64 \
+  https://github.com/cloudflare/cfssl/releases/download/v1.4.1/cfssljson_1.4.1_linux_amd64
 
-chmod +x cfssl_linux-amd64 cfssljson_linux-amd64
+chmod +x cfssl_1.4.1_linux_amd64 cfssljson_1.4.1_linux_amd64
 
-sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
-
-sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+sudo mv cfssl_1.4.1_linux_amd64 /usr/local/bin/cfssl
+sudo mv cfssljson_1.4.1_linux_amd64 /usr/local/bin/cfssljson
 
 cfssl version
 
-wget https://storage.googleapis.com/kubernetes-release/release/v1.26.13/bin/linux/amd64/kubectl
+wget https://storage.googleapis.com/kubernetes-release/release/v1.21.0/bin/linux/amd64/kubectl
 
 chmod +x kubectl
 
 sudo mv kubectl /usr/local/bin
 
-kubectl version --short
+kubectl version --client
 
 sudo apt install jq
 
